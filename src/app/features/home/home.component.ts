ed371a0c6a50af26a8173a3f6e84c09dd8aed4d3
irina-procopciuc <irina.procopciuc@mindit.io>
2023-04-07 17:30:21 +0300

    homepage banner and featured products

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as data from '../../api/products.json';
+import { Product } from 'src/app/shared/models/product';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  featuredProductsList: Product[] = [];
 
+  ngOnInit(): void {
+    this.featuredProductsList = (data as any).default;
+  }
 }
