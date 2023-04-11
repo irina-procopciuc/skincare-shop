7fe024cf586925a27729211105e6bf148b081a16
irina-procopciuc <irina.procopciuc@mindit.io>
2023-04-11 18:09:01 +0300

    finish homepage layout

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as data from '../../api/products.json';
+import * as promotionsData from '../../api/promotions.json';
 import { Product } from 'src/app/shared/models/product';
+import { Promotion } from 'src/app/shared/models/promotion';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +11,11 @@ import { Product } from 'src/app/shared/models/product';
 })
 export class HomeComponent implements OnInit {
   featuredProductsList: Product[] = [];
+  activePromotions: Promotion[] = [];
 
   ngOnInit(): void {
     this.featuredProductsList = (data as any).default;
+    this.activePromotions = (promotionsData as any).default;
+    this.activePromotions = this.activePromotions.filter(promotion => promotion.isActive === true);
   }
 }
